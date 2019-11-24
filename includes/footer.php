a490f7f9a2555df3274e8a871f948b94090b9be1
eliteraihan <56806721+eliteraihan@users.noreply.github.com>
2019-11-24 14:19:32 +0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<footer class="page-footer">
+    <div class="footer-copyright">
+      <div class="container">
+        <span>Copyright © 2019 <a class="grey-text text-lighten-4" href="#" target="_blank">Kedai Kopi</a> All rights reserved.</span>
+        <span class="right"> Design and Developed by <a class="grey-text text-lighten-4" href="#">Kedai Kopi</a></span>
+        </div>
+    </div>
+  </footer>
+  
